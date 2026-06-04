5c05d7e27e52d0ede433ef0f4c12503720093825
Anh Khoa Nguyễn <nguyenanhkhoa2135@gmail.com>
2026-06-05 01:38:50 +0700

    Update RouterLink for register Component

--- src/app/register/register.ts
+++ src/app/register/register.ts
@@ -2,23 +2,23 @@ import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
-import { Route, Router } from '@angular/router';
+import { Route, Router, RouterLink } from '@angular/router';
 import { CommonEngine } from '@angular/ssr/node';
 import { AuthService } from '../Services/auth-service';
 // Import Service gọi API của bạn ở đây (ví dụ: AuthService)
 
 @Component({
   selector: 'app-register',
-  imports: [CommonModule, FormsModule, ReactiveFormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, RouterLink],
   templateUrl: './register.html',
   styleUrls: ['./register.css']
 })
-export class RegisterComponent{
-  registerForm!:FormGroup;
+export class RegisterComponent {
+  registerForm!: FormGroup;
 
-  constructor(private fb:FormBuilder, private route:Router, private _authService: AuthService ){}
-  
-  ngOnInit(){
+  constructor(private fb: FormBuilder, private route: Router, private _authService: AuthService) { }
+
+  ngOnInit() {
     this.registerForm = this.fb.group({
       FullName: ['', Validators.required],
       UserName: ['', Validators.required],
@@ -30,15 +30,15 @@ export class RegisterComponent{
       Address: ['', Validators.required],
     });
   }
-  
-  onSubmit(){
+
+  onSubmit() {
     if (this.registerForm.invalid) {
       this.registerForm.markAllAsTouched();
       return;
     }
     this._authService.register(this.registerForm.value).subscribe(
       {
-        next:(res: any) => {
+        next: (res: any) => {
           alert(res.message || 'Đăng ký thành công!');
           this.route.navigate(['/login']);
         },
@@ -46,7 +46,7 @@ export class RegisterComponent{
           alert("Error: " + (err.error?.message || 'Có lỗi xảy ra.'));
         }
       }
-    );    
+    );
   }
 
 }
